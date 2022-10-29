7e202e957f301fed06c05d82b41b366fdc8f5873
Panitnun Suvannabun <Panitnun.6243@gmail.com>
2022-10-29 18:22:09 +0700

    Read data from textbox

--- app.py
+++ app.py
@@ -1,1 +1,6 @@
+import streamlit as st
+import pandas as pd
 
+st.title('CSV Data exploration with Github')
+text_data = st.text_input('Your github url', "", placeholder="e.g., https://raw.githubusercontent.com/dataprofessor/data/master/iris.csv")
+st.write(text_data)
