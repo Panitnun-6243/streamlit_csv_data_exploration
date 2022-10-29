5e0440ed592612b56358df5e4b59f467fd98e395
Panitnun Suvannabun <Panitnun.6243@gmail.com>
2022-10-29 18:23:31 +0700

    Show data as a dataframe

--- app.py
+++ app.py
@@ -3,4 +3,9 @@ import pandas as pd
 
 st.title('CSV Data exploration with Github')
 text_data = st.text_input('Your github url', "", placeholder="e.g., https://raw.githubusercontent.com/dataprofessor/data/master/iris.csv")
-st.write(text_data)
+if text_data == "":
+    st.info("We didn't receive any data, please type your github url in text box")
+else:
+    st.subheader("CSV data")
+    df = pd.read_csv(text_data)
+    st.write(df)
